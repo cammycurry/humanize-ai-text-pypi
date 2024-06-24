b105e379d9f821491a3045220b4880dcaa82bf12
cammycurry <camdcurry@gmail.com>
2024-06-24 01:10:21 -0500

    1.0.1

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="humanize-ai-text",
-    version="1.0.0",
+    version="1.0.1",
     author="Cam Curry",
     author_email="cam@humanize-ai-text.ai",
     description="SDK for Humanized AI Text API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/humanize-ai-text",
+    url="https://github.com/cammycurry/humanize-ai-text-pypi",
+    project_urls={
+        "Homepage": "https://humanize-ai-text.ai",
+        "Source": "https://github.com/cammycurry/humanize-ai-text-pypi",
+    },
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
